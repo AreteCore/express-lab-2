6b3a72a811adcb5d2f7da10753bc2d203ac9b93d
Richard Clarke <freshnewrich@gmail.com>
2022-04-02 17:41:40 -0700

    tip calculator express application created

--- server.js
+++ server.js
@@ -10,4 +10,9 @@ app.get("/greeting/:name", function(req,res) {
     res.send(`What's up, ${req.params.name}?!`)
 })
 
+//http://localhost:3000/tip/100/10
+app.get("/tip/:total/:tipPercentage", function(req,res) {
+    res.send(`${parseInt(req.params.total * (parseInt(req.params.tipPercentage)/100 + 1))}`)
+})
+
 
